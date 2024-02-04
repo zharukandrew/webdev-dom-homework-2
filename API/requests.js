67401b066d62498ac419631466d9927ef9415f7a
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2024-02-04 17:39:34 +0400

    I did one homework

--- API/requests.js
+++ API/requests.js
@@ -3,19 +3,21 @@ import { renderComments } from '../js/render.js';
 import { comments } from '../js/localData.js';
 
 let isLoad = false;
+let token = `Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k`
+const host = "https://wedev-api.sky.pro/api/v2/andrey-zharuck/comments";
 
-
-
+ 
 const getData = async () => {
     isLoad = true;
     showForm(isLoad);
     try {
       let response = await fetch(
-        "https://wedev-api.sky.pro/api/v1/:andrey-zharuck/comments",
+        host,
         {
           method: "GET",
           headers: {
             "Content-Type": "application/activity+json",
+            Authorization:token,
           },
         }
       );
@@ -42,17 +44,34 @@ const getData = async () => {
     } catch (error) {
       alert(`Error adding comment: ${error}` );
     }
+    // Второй участок кода
+    return fetch(host,)
+        .then((response) => {
+            if (response.status === 401) {
+                password = prompt("Введите верный пароль");
+                throw new Error("Нет авторизации");
+              
+                
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error(error);
+        });
   };
 
 
   
+  
+
   function PostComment(newComment) {
-    fetch("https://wedev-api.sky.pro/api/v1/:andrey-zharuck/comments", {
+    fetch(host, {
       method: "POST",
       headers: {
         "Content-Type": "application/activity+json",
+        Authorization:token,
       },
-      body: JSON.stringify({ name: newComment.name, text: newComment.text, forceError: true }),
+      body: JSON.stringify({ name: newComment.name, text: newComment.text,forceError: true }),
     })
       .then((response) => {
         if (response.status === 400) {
@@ -71,5 +90,4 @@ const getData = async () => {
         alert("Неполадки с интернетом. Пожалуйста, проверьте соединение.", error);
       });
   }
-
   export { getData, PostComment }
